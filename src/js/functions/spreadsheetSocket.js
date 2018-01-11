aa103169faa337a0844bbac3a9ade090725af3b9
ddaaggeett <david.g.daggett@gmail.com>
2018-01-10 22:05:32 -0500

    -

--- src/js/functions/spreadsheetSocket.js
+++ src/js/functions/spreadsheetSocket.js
@@ -15,7 +15,7 @@
 *   along with this program; if not, write to the Free Software Foundation,
 *   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
-import client_data from '../../assets/restaurant/clients'
+import client_data from '../../../configs/restaurant/clients'
 var gsjson = require('google-spreadsheet-to-json');
 
 export const spreadsheetSocket = (app) => {
